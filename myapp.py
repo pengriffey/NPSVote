7bf3b63ef2002815f6c4b2316dfd0b6ab9a9af3b
shruthi63 <shruthi63@infoxsystems.com>
2016-10-30 11:24:00 +0530

    yes

---
+++ myapp.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import request, render_template, redirect,  url_for
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:shruthi63@localhost/NPSElect'
+db = SQLAlchemy(app)
+app.debug=True
+
+
+class Votes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    hg = db.Column(db.String(80))
+    hb = db.Column(db.String(120))
+
+    def __init__(self,hg, hb):
+        self.hg = hg
+        self.hb = hb
+
+    def __repr__(self):
+        return '<User %r>' % self.hb
+
+
+@app.route('/')
+def index():
+    return render_template('vote.html')
+
+
+@app.route('/nps_vote', methods=['POST'])
+def post_user():
+    vote = Votes(request.form['hg'], request.form['hb'])
+    db.session.add(vote)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    app.run()
